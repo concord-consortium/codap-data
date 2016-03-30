2712fc6eff1675384cf9e25bd0d02602f095673c
William Finzer <wfinzer@concord.org>
2016-03-30 16:35:33 -0700

    Adding this workshop web_page site so participants can access it after the fact.

---
+++ nsta_codap/cc-page/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>concord.org | NEACT Summer 2014 Links</title>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen,projection" />
+<link rel="icon" type="image/png" href="favicon.ico">
+<script type="text/javascript" src="http://use.typekit.com/hdw8ayt.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+</head>
+<body>
+<div id="wrapper">
+<div id="headerbg"><div id="header">
+    <h1>NEACT Summer 2015 - NGSS Curriculum</h1>
+</div></div>
+  <div id="main">
+
+<div id="maindiv">
+<p align=center>
+<img id="cclogo" src="cc-logo.png"></p>
+<p align=center>&nbsp;</p>
+<p align=center>This page available online at:<a href="http://short.concord.org/3a" target="_new">http://short.concord.org/3a</a></p>
+<p align=center><br>
+</p>
+<hr><br>
+
+<h3>Interactions curriculum</h3>
+<ul>
+  <li>Interactions portal (collection of online investigations): <a href="http://interactions.portal.concord.org" target="_blank">http://interactions.portal.concord.org</a></li>
+  <li>NSTA NGSS Video series featuring the Interactions curriculum: <a href="http://ngss.nsta.org/ngss-videos.aspx" target="_blank">http://ngss.nsta.org/ngss-videos.aspx</a></li>
+</ul>
+</br>
+
+<h3>Other Resources at the Concord Consortium</h3>
+<ul>
+  <li>NGSS Pathways finder - <a href="http://concord.org/ngss/" target="_new">http://concord.org/ngss/</a></li>
+  <li>The STEM resources finder - <a href="http://concord.org/stem-resources" target="_new">http://concord.org/stem-resources</a></li>
+  <li>Next Generation Molecular Workbench site: <a href="http://mw.concord.org/nextgen" title="NextGen MW" target="_new">http://mw.concord.org/nextgen</a></li>
+  <li>Interactives/models full listing (including drafts): <a href="http://lab.concord.org/interactives.html" target="_new">http://lab.concord.org/interactives.html</a></li>
+</ul>
+</br>
+<h3>User manuals and guides</h3>
+<ul>
+
+<li>Embedding interactives into your own courseware: <a href="http://short.concord.org/1j" target="_new">http://short.concord.org/1j</a></li>
+<li>Portal user guide:<a href="http://short.concord.org/2a" target="_new">http://short.concord.org/2a</a><br>
+</li>
+</ul>
+</br>
+<h3>Selected Interactive Models Embedded in the Interactions Curruiculum</h3>
+<ul>
+  <li>3D Exploration of Bound Antibody &amp; Antigen- <a href="http://lab.concord.org/interactives.html#interactives/interactions/3D-antibody.json" target="_blank">http://lab.concord.org/interactives.html#interactives/interactions/3D-antibody.json</a></li>
+  <li>Comparing boiling point of polar &amp; nonpolar - <br>
+    <a href="http://lab.concord.org/interactives.html#interactives/interactions/boiling-point-polar-nonpolar.json" target="_blank">http://lab.concord.org/interactives.html#interactives/interactions/boiling-point-polar-nonpolar.json</a> </li>
+  <li>Breaking a Molecular Bond - <br>
+    <a href="http://lab.concord.org/interactives.html#interactives/interactions/breaking-molecular-bond.json" target="_blank">http://lab.concord.org/interactives.html#interactives/interactions/breaking-molecular-bond.json</a></li>
+  <li>Making Molecules: Exploring polarization -<br>
+    <a href="http://lab.concord.org/interactives.html#interactives/interactions/buildMoleculesPartialCharge.json" target="_blank">http://lab.concord.org/interactives.html#interactives/interactions/buildMoleculesPartialCharge.json</a></li>
+  <li>Comparing Potential Energy of Intermolecular Attractions and Chemical Bonds -<br>
+    <a href="http://lab.concord.org/interactives.html#interactives/interactions/comparing-potential-energy-of-bond.json" target="_blank">http://lab.concord.org/interactives.html#interactives/interactions/comparing-potential-energy-of-bond.json</a> </li>
+  <li>Forming a Molecule - <br>
+    <a href="http://lab.concord.org/interactives.html#interactives/interactions/forming-molecule.json" target="_blank">http://lab.concord.org/interactives.html#interactives/interactions/forming-molecule.json</a> </li>
+  <li>Energy of Bond Formation - <br>
+    <a href="http://lab.concord.org/interactives.html#interactives/interactions/forming-molecules-graph.json" target="_blank">http://lab.concord.org/interactives.html#interactives/interactions/forming-molecules-graph.json</a> <br>
+  </li>
+  <li>Atom and Ion Builder - <a href="http://lab.concord.org/interactives.html#interactives/interactions/atom-builder.json" target="_new">http://lab.concord.org/interactives.html#interactives/interactions/atom-builder.json</a></li>
+  <li>Balloon and Wall (polarization) - <a href="http://lab.concord.org/interactives.html#interactives/interactions/balloonModelElectrons.json" target="_new">http://lab.concord.org/interactives.html#interactives/interactions/balloonModelElectrons.json</a></li>
+  <li>Crookes Tube Simulation - <a href="http://lab.concord.org/interactives.html#interactives/interactions/crookesElectrodes.json" target="_new">http://lab.concord.org/interactives.html#interactives/interactions/crookesElectrodes.json</a></li>
+  <li>Focusing on Cathode Rays - <a href="http://lab.concord.org/interactives.html#interactives/interactions/electronProperties.json" target="_new">http://lab.concord.org/interactives.html#interactives/interactions/electronProperties.json</a></li>
+  <li>Simple polarization - <a href="http://lab.concord.org/interactives.html#interactives/interactions/deformedCloud.json" target="_new">http://lab.concord.org/interactives.html#interactives/interactions/deformedCloud.json</a></li>
+  <li>Understanding probability maps - <a href="http://lab.concord.org/interactives.html#interactives/interactions/electron-darts.json" target="_new">http://lab.concord.org/interactives.html#interactives/interactions/electron-darts.json</a></li>
+  <li>Particle model (mixing liquids) - <br>
+    <a href="http://lab.concord.org/interactives.html#interactives/interactions/Mixing-polar-nonpolar-particle.json" target="_blank">http://lab.concord.org/interactives.html#interactives/interactions/Mixing-polar-nonpolar-particle.json</a></li>
+  <li>Particle model (syringe) - <a href="http://lab.concord.org/interactives.html#interactives/interactions/syringe.json" target="_new">http://lab.concord.org/interactives.html#interactives/interactions/syringe.json</a></li>
+  <li>Rutherford simulation - <a href="http://lab.concord.org/interactives.html#interactives/interactions/rutherford-no-electrons.json" target="_blank">http://lab.concord.org/interactives.html#interactives/interactions/rutherford-no-electrons.json</a></li>
+  <li>Concentration and reaction rate - <a href="http://lab.concord.org/interactives.html#interactives/sam/chemical-reactions/2-concentration-and-reaction-rate.json" target="_new">http://lab.concord.org/interactives.html#interactives/sam/chemical-reactions/2-concentration-and-reaction-rate.json</a></li>
+  <li>Making water - <a href="http://lab.concord.org/interactives.html#interactives/sam/chemical-reactions/5-hydrogen-oxygen-reaction.json" target="_new">http://lab.concord.org/interactives.html#interactives/sam/chemical-reactions/5-hydrogen-oxygen-reaction.json</a></li>
+</ul>
+<p>&nbsp;</p>
+<h3>Stay in Touch</h3>
+<ul>
+  <li>Subscribe to our newsletter: <a href="http://concord.org/subscribe" target="_blank">http://www.concord.org/subscribe</a></li>
+  <li>Facebook: <a href="http://facebook.com/concordconsortium" target="_blank">http://facebook.com/concordconsortium</a></li>
+  <li>Twitter: @ConcordDotOrg @chaddorsey</li>
+  <li>Dan Damelin: ddamelin@concord.org</li>
+</ul>
+<p><br>
+</p>
+</div>
+  </div></div>
+</body>
+</html>
