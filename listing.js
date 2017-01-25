621dc2640f7d643412fba77061b35552c888cf4b
Evangeline Ireland <eireland@concord.org>
2017-01-24 20:29:38 -0800

    Initial commit of site overhaul to add search and filtering instead of just a list of sample documents

--- listing.js
+++ listing.js
@@ -43,154 +43,158 @@ $(document).ready(function () {
         return (cat_relationship_obj);
     }
 
-    function buildNavBar(categories){
-        console.log("In buildNavBar, categories is: " + categories);
-        var nav_bar = $(".nav-bar");
-        var nav_bar_list = $("<ul>").addClass('menu-h');
-        var cat_main_header, cat_sub_header;
-
-        nav_bar_list.appendTo(nav_bar);
-        for (var key in categories) {
-            if (key.match(' ')){
-                cat_main_header = key.replace(/ /g, '_');
-            } else {
-                cat_main_header=key;
-            }
-
-            nav_bar_list.append($('<li>').addClass('main_cat_'+cat_main_header).append($('<a>').attr('href', '#'+cat_main_header+'_header').text(key)));
-
-            var sub_headers = categories[key];
-            if(!(sub_headers===undefined)) {
-                $('.main_cat_'+cat_main_header).append($('<ul>').addClass('menu-h-dropdown sub_cat_'+cat_main_header));
-                sub_headers.forEach(function (el) {
-                    if(!(el===undefined)) {
-                        if (el.match(' ')) {
-                            cat_sub_header = el.replace(/ /g, '_');
-                        } else {
-                            cat_sub_header = el;
-                        }
-                        $('.sub_cat_'+cat_main_header).append($('<li>').append($('<a>').attr('href', '#'+cat_sub_header + '_header').text(el)));
-                    }
-                })
-            }
-
-        }
+    function searchResources() {
+        console.log("in searchResources");
     }
 
-    function buildListingDivs(categories) {
-        console.log("In buildListingDivs");
-        var cat_list='';
-        var cat_main_header='';
-        var cat_sub_header='';
-        var listing_container = $("#listing_container");
-        var home_button = $('<a>').attr('href','#header').addClass('nav-to-top').text('Back to top').append('<img src="./Common/images/home.png" class="home-icon">');
-
-
-        for (var key in categories) {
-            if (key.match(' ')){
-                cat_main_header = key.replace(/ /g, '_');
-            } else {
-                cat_main_header=key;
-            }
-
-            var cat_listing_container = $('<div>').addClass('cat_listing_container').addClass(cat_main_header+'_div');
-            listing_container.append(cat_listing_container);
-            $('.'+cat_main_header+'_div').append($('<h2>').text(key).prop('id',cat_main_header+'_header'));
-            $('.'+cat_main_header+'_div').append($('<ul>').prop('id',cat_main_header+'_list'));
-            cat_list = $('#'+cat_main_header+'_list');
-
-            var sub_headers = categories[key];
-            console.log(sub_headers);
-                sub_headers.forEach(function (el) {
-                    if (!(el===undefined)) {
-                    if (el.match(' ')) {
-                        cat_sub_header = el.replace(/ /g, '_');
-                    } else {
-                        cat_sub_header = el;
-                    }
-                    cat_list.append($('<h3>').text(el).prop('id',cat_sub_header+'_header'));
-                    cat_list.append($('<ul>').prop('id', cat_sub_header + '_list'));
-                }
-                });
-        }
-        $('.cat_listing_container').append(home_button);
-         buildNavBar(categories);
-
-    }
-
-    function AddListingObj(obj) {
-        var title = obj.title,
-            description = obj.description,
-            path = '',
-            category = obj.categories,
-            url = $("#codap-url").val(),
-            category_bin ='',
-            listing = '',
-            listing_desc ='',
-            query_param = '?url=',
-            launchLink = '',
-            linkLink = '',
-            url_root = window.location.origin+window.location.pathname;
-
-        if (obj.path.match('^http','i')) {
-            path = obj.path;
-        }
-        else {
-            url_root=url_root.replace(/index.html$/, '');
-            path = url_root+obj.path;
-        }
-
-        if (url.match(/^https/i) && !path.match(/^https/i)) {
-            path=path.replace(/http/i,'https');
-        }
-
-        for (var i=0; i<category.length;i++) {
-            if (category[i].includes('/')) {
-                console.log("In category split");
-
-                var category_split = category[i].split("/");
-                if (/ /g.test(category_split[1])){
-                    category_split[1] = category_split[1].replace(/ /g, '_');
-                }
-                category_bin = '#' + category_split[1]+'_list';
-            }
-            else {
-                if (/ /g.test(category[i])){
-                    category[i] = category[i].replace(/ /g, '_');
-                }
-                category_bin = '#' + category[i] + '_list';
-            }
-            listing = $('<li>').addClass('listing');
-             launchLink = $('<a class = "listing-title" target = "_blank" href='+url+query_param+path+'> '+title+' </a>'),
-                 listing_desc = $('<p>').addClass('listing-desc').text(description),
-                 linkLink = $('<a class = "listing-link" href=' + path + '> Embeddable Link </a>'),
-             launchLink.appendTo(listing);
-            listing_desc.appendTo(listing);
-             linkLink.appendTo(listing);
-             listing.appendTo(category_bin);
-        }
-    }
-
-
-    function buildListing(listing){
-        //check if item is visible
-        $('.listing').remove();
-        for (var i=0; i<listing.length; i++) {
-            if (listing[i].visible) {
-                AddListingObj(listing[i]);
-            }
-        }
-    }
-
-    function buildPage(response) {
-        var categories_obj = getCategories(response);
-        var sample_doc_list = response.sample_docs;
-        buildListingDivs(categories_obj);
-        buildListing(sample_doc_list);
-
-        $('form').submit(function(){buildListing(sample_doc_list); return false;});
-
-    }
+    // function buildNavBar(categories){
+    //     console.log("In buildNavBar, categories is: " + categories);
+    //     var nav_bar = $(".nav-bar");
+    //     var nav_bar_list = $("<ul>").addClass('menu-h');
+    //     var cat_main_header, cat_sub_header;
+    //
+    //     nav_bar_list.appendTo(nav_bar);
+    //     for (var key in categories) {
+    //         if (key.match(' ')){
+    //             cat_main_header = key.replace(/ /g, '_');
+    //         } else {
+    //             cat_main_header=key;
+    //         }
+    //
+    //         nav_bar_list.append($('<li>').addClass('main_cat_'+cat_main_header).append($('<a>').attr('href', '#'+cat_main_header+'_header').text(key)));
+    //
+    //         var sub_headers = categories[key];
+    //         if(!(sub_headers===undefined)) {
+    //             $('.main_cat_'+cat_main_header).append($('<ul>').addClass('menu-h-dropdown sub_cat_'+cat_main_header));
+    //             sub_headers.forEach(function (el) {
+    //                 if(!(el===undefined)) {
+    //                     if (el.match(' ')) {
+    //                         cat_sub_header = el.replace(/ /g, '_');
+    //                     } else {
+    //                         cat_sub_header = el;
+    //                     }
+    //                     $('.sub_cat_'+cat_main_header).append($('<li>').append($('<a>').attr('href', '#'+cat_sub_header + '_header').text(el)));
+    //                 }
+    //             })
+    //         }
+    //
+    //     }
+    // }
+    //
+    // function buildListingDivs(categories) {
+    //     console.log("In buildListingDivs");
+    //     var cat_list='';
+    //     var cat_main_header='';
+    //     var cat_sub_header='';
+    //     var listing_container = $("#listing_container");
+    //     var home_button = $('<a>').attr('href','#header').addClass('nav-to-top').text('Back to top').append('<img src="./Common/images/home.png" class="home-icon">');
+    //
+    //
+    //     for (var key in categories) {
+    //         if (key.match(' ')){
+    //             cat_main_header = key.replace(/ /g, '_');
+    //         } else {
+    //             cat_main_header=key;
+    //         }
+    //
+    //         var cat_listing_container = $('<div>').addClass('cat_listing_container').addClass(cat_main_header+'_div');
+    //         listing_container.append(cat_listing_container);
+    //         $('.'+cat_main_header+'_div').append($('<h2>').text(key).prop('id',cat_main_header+'_header'));
+    //         $('.'+cat_main_header+'_div').append($('<ul>').prop('id',cat_main_header+'_list'));
+    //         cat_list = $('#'+cat_main_header+'_list');
+    //
+    //         var sub_headers = categories[key];
+    //         console.log(sub_headers);
+    //             sub_headers.forEach(function (el) {
+    //                 if (!(el===undefined)) {
+    //                 if (el.match(' ')) {
+    //                     cat_sub_header = el.replace(/ /g, '_');
+    //                 } else {
+    //                     cat_sub_header = el;
+    //                 }
+    //                 cat_list.append($('<h3>').text(el).prop('id',cat_sub_header+'_header'));
+    //                 cat_list.append($('<ul>').prop('id', cat_sub_header + '_list'));
+    //             }
+    //             });
+    //     }
+    //     $('.cat_listing_container').append(home_button);
+    //      buildNavBar(categories);
+    //
+    // }
+    //
+    // function AddListingObj(obj) {
+    //     var title = obj.title,
+    //         description = obj.description,
+    //         path = '',
+    //         category = obj.categories,
+    //         url = $("#codap-url").val(),
+    //         category_bin ='',
+    //         listing = '',
+    //         listing_desc ='',
+    //         query_param = '?url=',
+    //         launchLink = '',
+    //         linkLink = '',
+    //         url_root = window.location.origin+window.location.pathname;
+    //
+    //     if (obj.path.match('^http','i')) {
+    //         path = obj.path;
+    //     }
+    //     else {
+    //         url_root=url_root.replace(/index.html$/, '');
+    //         path = url_root+obj.path;
+    //     }
+    //
+    //     if (url.match(/^https/i) && !path.match(/^https/i)) {
+    //         path=path.replace(/http/i,'https');
+    //     }
+    //
+    //     for (var i=0; i<category.length;i++) {
+    //         if (category[i].includes('/')) {
+    //             console.log("In category split");
+    //
+    //             var category_split = category[i].split("/");
+    //             if (/ /g.test(category_split[1])){
+    //                 category_split[1] = category_split[1].replace(/ /g, '_');
+    //             }
+    //             category_bin = '#' + category_split[1]+'_list';
+    //         }
+    //         else {
+    //             if (/ /g.test(category[i])){
+    //                 category[i] = category[i].replace(/ /g, '_');
+    //             }
+    //             category_bin = '#' + category[i] + '_list';
+    //         }
+    //         listing = $('<li>').addClass('listing');
+    //          launchLink = $('<a class = "listing-title" target = "_blank" href='+url+query_param+path+'> '+title+' </a>'),
+    //              listing_desc = $('<p>').addClass('listing-desc').text(description),
+    //              linkLink = $('<a class = "listing-link" href=' + path + '> Embeddable Link </a>'),
+    //          launchLink.appendTo(listing);
+    //         listing_desc.appendTo(listing);
+    //          linkLink.appendTo(listing);
+    //          listing.appendTo(category_bin);
+    //     }
+    // }
+    //
+    //
+    // function buildListing(listing){
+    //     //check if item is visible
+    //     $('.listing').remove();
+    //     for (var i=0; i<listing.length; i++) {
+    //         if (listing[i].visible) {
+    //             AddListingObj(listing[i]);
+    //         }
+    //     }
+    // }
+    //
+    // function buildPage(response) {
+    //     var categories_obj = getCategories(response);
+    //     var sample_doc_list = response.sample_docs;
+    //     buildListingDivs(categories_obj);
+    //     buildListing(sample_doc_list);
+    //
+    //     $('form').submit(function(){buildListing(sample_doc_list); return false;});
+    //
+    // }
 
     fetchObjList();
 });
