c9187cb856bf2911920a9a4f5635dc8dcd1b7ef3
Evangeline Ireland <eireland@concord.org>
2017-01-31 15:28:32 -0800

    Added functionality to the search function. Added code to add word_list property to sample_doc_list to be used in the search function. The word_list property is the list of words in the title and the description.

--- listing.js
+++ listing.js
@@ -13,35 +13,60 @@ $(document).ready(function () {
         })
     }
 
+    function removeCommonWords(words) {
+        var word_list = [],
+            ignore_words = ["the", "it", "is", "we", "all", "a", "an", "by", "to", "you", "at", "she", "they", "we", "how", "it", "i", "are", "to", "for", "of", "in", "that", "and"];
+
+        console.log("In removeCommonWords");
+        console.log("Words are: "+words);
+
+        for (var k=0;k<words.length;k++) {
+            console.log("Word[k] is: "+words[k]);
+            if (!(ignore_words.includes(words[k]))) {
+                word_list.push(words[k]);
+            }
+        }
+        console.log("Word list is: " + word_list);
+        return word_list;
+    }
+
     function searchResources(obj) {
         console.log("in searchResources");
-        var title='',
-            description='',
-            tags = [],
+        var tags = [],
+            words=[],
             display_list=[],
-            search_word = ($('#keyword').val()).toLowerCase();
+            search_words=[],
+            keywords = ($('#keyword').val()).toLowerCase(),
+            space=" ";
 
-        console.log("Search_word is "+ search_word);
+        search_words = keywords.split(space);
+        console.log("Search_word is "+ search_words);
 
         for (var i=0; i<obj.length-1;i++) {
-            title = (obj[i].title).toLowerCase();
-            description = (obj[i].description).toLowerCase();
             tags = obj[i].tag;
+            words = obj[i].word_list;
 
-            if (typeof tags != "undefined" && tags != null && tags.length > 0){
-                if (tags.includes(search_word)){
-                    display_list.push(obj[i]);
+            for (var j=0;j<search_words.length-1;j++) {
+                if (typeof tags != "undefined" && tags != null && tags.length > 0) {
+
+                    if (tags.includes(search_words[j])) {
+                        display_list.push(obj[i]);
+                    }
+
                 }
-             }
-            if ((title.includes(search_word)) || (description.includes(search_word))) {
-                console.log(obj[i].title + " should be displayed");
-                if (!(display_list.includes(obj[i]))) {
-                    display_list.push(obj[i]);
+                if (typeof words != "undefined" && words != null && words.length > 0) {
+                    if (words.includes(search_words[j])) {
+                        console.log(obj[i].title + " should be displayed");
+                        if (!(display_list.includes(obj[i]))) {
+                            display_list.push(obj[i]);
+                        }
+                    }
                 }
             }
         }
 
         buildListing(display_list);
+
     }
 
     function filterCategory() {
@@ -108,10 +133,19 @@ $(document).ready(function () {
     function buildListing(listing){
         console.log("in buildListing");
         //check if item is visible
+
         $('.listing').remove();
-        for (var i=0; i<listing.length; i++) {
-            if (listing[i].visible) {
-                AddListingObj(listing[i]);
+        $('.error').remove();
+
+        if (typeof listing == "undefined" || listing == null || listing.length <= 0) {
+            $('#listing_container').append('<p class="error">Sorry, no document matches your search.</p>');
+        }
+        else {
+            $('.error').remove();
+            for (var i = 0; i < listing.length; i++) {
+                if (listing[i].visible) {
+                    AddListingObj(listing[i]);
+                }
             }
         }
     }
@@ -119,6 +153,24 @@ $(document).ready(function () {
     function buildPage(response) {
         console.log("in buildPage");
         var sample_doc_list = response.sample_docs;
+        var title='',
+            description='',
+            space = " ",
+            words=[],
+            word_list=[];
+
+
+        for (var i=0; i<sample_doc_list.length-1;i++) {
+            title = (((sample_doc_list[i].title).toLowerCase()).replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")).trim();
+            description = (((sample_doc_list[i].description).toLowerCase()).replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")).trim();
+            words = (title.split(space)+',' + description.split(space)).split(',');
+
+            word_list = removeCommonWords(words);
+
+            console.log("Word list is: " + word_list);
+            sample_doc_list[i].word_list = word_list;
+        }
+
         buildListing(sample_doc_list);
 
         $("#search").submit(function() {searchResources(sample_doc_list); return false;});
