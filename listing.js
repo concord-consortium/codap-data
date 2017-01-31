57718663d352e858149186ad8cebc8d4d0339da7
Evangeline Ireland <eireland@concord.org>
2017-01-30 22:39:43 -0800

    Added subject categories as classes to listings

--- listing.js
+++ listing.js
@@ -13,123 +13,36 @@ $(document).ready(function () {
         })
     }
 
-    function getCategories (obj){
-        console.log("In getCategories");
-        var categories_array=[],
-            category_objects=[],
-            cat_relationship_obj={};
 
-        for (var i=0; i < obj.sample_docs.length; i++) {
-            for (var j=0; j < obj.sample_docs[i].categories.length; j++) {
-                var category_read = obj.sample_docs[i].categories[j];
-                if (!categories_array.includes(category_read) ) {
-                    categories_array.push(category_read);
-                }
-            }
-        }
-        category_objects = categories_array.map(function (s) {
-            var split=s.split('/');
-            return {parent:split[0],child:split[1]};
-        });
-
-        for (i=0;i<category_objects.length;i++) {
-            if (!cat_relationship_obj[category_objects[i].parent]){
-                cat_relationship_obj[category_objects[i].parent]=[category_objects[i].child];
-            }
-            else {
-                cat_relationship_obj[category_objects[i].parent].push(category_objects[i].child);
-            }
-        }
-        return (cat_relationship_obj);
-    }
 
     function searchResources() {
         console.log("in searchResources");
     }
 
-    // function buildNavBar(categories){
-    //     console.log("In buildNavBar, categories is: " + categories);
-    //     var nav_bar = $(".nav-bar");
-    //     var nav_bar_list = $("<ul>").addClass('menu-h');
-    //     var cat_main_header, cat_sub_header;
-    //
-    //     nav_bar_list.appendTo(nav_bar);
-    //     for (var key in categories) {
-    //         if (key.match(' ')){
-    //             cat_main_header = key.replace(/ /g, '_');
-    //         } else {
-    //             cat_main_header=key;
-    //         }
-    //
-    //         nav_bar_list.append($('<li>').addClass('main_cat_'+cat_main_header).append($('<a>').attr('href', '#'+cat_main_header+'_header').text(key)));
-    //
-    //         var sub_headers = categories[key];
-    //         if(!(sub_headers===undefined)) {
-    //             $('.main_cat_'+cat_main_header).append($('<ul>').addClass('menu-h-dropdown sub_cat_'+cat_main_header));
-    //             sub_headers.forEach(function (el) {
-    //                 if(!(el===undefined)) {
-    //                     if (el.match(' ')) {
-    //                         cat_sub_header = el.replace(/ /g, '_');
-    //                     } else {
-    //                         cat_sub_header = el;
-    //                     }
-    //                     $('.sub_cat_'+cat_main_header).append($('<li>').append($('<a>').attr('href', '#'+cat_sub_header + '_header').text(el)));
-    //                 }
-    //             })
-    //         }
-    //
-    //     }
-    // }
-    //
-    // function buildListingDivs(categories) {
-    //     console.log("In buildListingDivs");
-    //     var cat_list='';
-    //     var cat_main_header='';
-    //     var cat_sub_header='';
-    //     var listing_container = $("#listing_container");
-    //     var home_button = $('<a>').attr('href','#header').addClass('nav-to-top').text('Back to top').append('<img src="./Common/images/home.png" class="home-icon">');
-    //
-    //
-    //     for (var key in categories) {
-    //         if (key.match(' ')){
-    //             cat_main_header = key.replace(/ /g, '_');
-    //         } else {
-    //             cat_main_header=key;
-    //         }
-    //
-    //         var cat_listing_container = $('<div>').addClass('cat_listing_container').addClass(cat_main_header+'_div');
-    //         listing_container.append(cat_listing_container);
-    //         $('.'+cat_main_header+'_div').append($('<h2>').text(key).prop('id',cat_main_header+'_header'));
-    //         $('.'+cat_main_header+'_div').append($('<ul>').prop('id',cat_main_header+'_list'));
-    //         cat_list = $('#'+cat_main_header+'_list');
-    //
-    //         var sub_headers = categories[key];
-    //         console.log(sub_headers);
-    //             sub_headers.forEach(function (el) {
-    //                 if (!(el===undefined)) {
-    //                 if (el.match(' ')) {
-    //                     cat_sub_header = el.replace(/ /g, '_');
-    //                 } else {
-    //                     cat_sub_header = el;
-    //                 }
-    //                 cat_list.append($('<h3>').text(el).prop('id',cat_sub_header+'_header'));
-    //                 cat_list.append($('<ul>').prop('id', cat_sub_header + '_list'));
-    //             }
-    //             });
-    //     }
-    //     $('.cat_listing_container').append(home_button);
-    //      buildNavBar(categories);
-    //
-    // }
-    //
+    function filterCategory() {
+        console.log("in filterCategory");
+            var id=this.id;
+            var filters=[];
+            //if no boxes are checked, show all, break
+            //else hide all listings
+            // $('.listing').hide();
+            //check which checkboxes are checked
+        console.log("id is "+id);
+            // $('"#'+id+'"').toggle(this.checked);
+            //else add checked ones to array
+            //show classes in array
+            $('.Math').show();
+    }
+
     function AddListingObj(obj) {
         var title = obj.title,
             description = obj.description,
             path = '',
-            category = obj.categories,
+            categories = obj.categories,
             url = $("#codap-url").val(),
             category_bin ='',
             listing = '',
+            listing_category='',
             listing_desc ='',
             query_param = '?url=',
             launchLink = '',
@@ -138,6 +51,7 @@ $(document).ready(function () {
             listing_container = "#listing_container";
 
         console.log("in AddListingObj");
+        console.log(categories);
         if (obj.path.match('^http','i')) {
             path = obj.path;
         }
@@ -150,7 +64,10 @@ $(document).ready(function () {
             path=path.replace(/http/i,'https');
         }
 
-        listing = $('<li>').addClass('listing');
+        listing = $('<li>').addClass('listing').addClass(categories);
+        // categories.forEach(function(category)
+        //     {   console.log(category);
+        //         $('li.listing').addClass(category)} );
         launchLink = $('<a class = "listing-title" target = "_blank" href='+url+query_param+path+'> '+title+' </a>'),
             listing_desc = $('<p>').addClass('listing-desc').text(description),
             linkLink = $('<a class = "listing-link" href=' + path + '> Embeddable Link </a>'),
@@ -175,11 +92,10 @@ $(document).ready(function () {
 
     function buildPage(response) {
         console.log("in buildPage");
-        var categories_obj = getCategories(response);
         var sample_doc_list = response.sample_docs;
-        // buildListingDivs(categories_obj);
         buildListing(sample_doc_list);
 
+        $("input[type=checkbox]").on("click",filterCategory());
         // $('form').submit(function(){buildListing(sample_doc_list); return false;});
 
     }
