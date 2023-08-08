318d4893d8d0b7b6efa438b79c2924007d15a2d5
Dan Damelin <ddamelin@concord.org>
2023-08-08 11:16:29 -0400

    Added list of example files for Polish

---
+++ example-documents/src/index.json.pl
@@ -0,0 +1,38 @@
+[
+  {
+    "name": "Getting started with CODAP",
+    "description": "Learn how to use CODAP using the CODAP plugin onboarding",
+    "location": "./documents/get_started.codap"
+  },
+  {
+    "name": "Getting started with CODAP 2",
+    "description": "More basics of CODAP",
+    "location": "./documents/get_started_2.codap"
+  },
+  {
+    "name": "Staw z kaczkami",
+    "description": "Zbadaj prawdopodobieństwo poprzez symulację loterii fantowej przy użyciu Próbnika.",
+    "location": "./documents/Staw-z-kaczkami.codap"
+  },
+  {
+    "name": "Dane na mapie",
+    "description": "Poznaj dane dotyczące populacji Stanów Zjednoczonych i zobacz, co odkryjesz.",
+    "location": "./documents/Dane-na-mapie.codap"
+  },
+  {
+    "name": "Rollercoastery",
+    "description": "Dane dotyczące 157 kolejek górskich w USA, które zostały otwarte w latach 1915-2016.",
+    "location": "./documents/Rollercoastery.codap"
+  },
+  {
+    "name": "Ssaki",
+    "description": "Badaj ssaki i odkrywaj interesujące zależności, korzystając z wykresów.",
+    "location": "./documents/Ssaki.codap"
+  },
+  {
+    "name": "Cztery foki",
+    "description": "Dane śledzenia mirungi naniesione na mapę. Utwórz wykresy, aby zbadać czynniki, które mogą wpływać na ich trasy.",
+    "location": "./documents/Cztery-foki.codap"
+  }
+]
+
